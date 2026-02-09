32df6ed605e43382a29b80ec24d04858147c2413
bkh1423 <bkh14231423@gmail.com>
2026-02-09 16:57:36 +0300

    تجربه

--- scans/views.py
+++ scans/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+def scan_test(request):
+    target = None
+
+    if request.method == "POST":
+        target = request.POST.get("target")
+
+    return render(request, "scans/scan_test.html", {
+        "target": target
+    })
--- scans/web_urls.py
+++ scans/web_urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import scan_test
 
 urlpatterns = [
-    path("test/", scan_test, name="scan-test"),
+    path('test/', scan_test, name='scan-test'),
 ]
